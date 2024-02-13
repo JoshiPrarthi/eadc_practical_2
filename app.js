ecc635a81840fc0cdbc3a2cb2423e3075a6fbcd1
Prarthi Joshi <prarthikjoshi21@gnu.ac.in>
2024-02-13 18:35:19 +0530

    “message”

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => {
-    res.send('Hello, this is a code by Prarthi to test pipeline and this is the second version');
-  });
-  const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`API server listening on port ${port}`);
+var express = require('express');
+var app = express();
+const port = process.env.PORT || 8000
+app.use(express.static(__dirname + '/prarthi'));
+app.listen(port,function() {
+// console.log("server starting on " + 8000);
 });
